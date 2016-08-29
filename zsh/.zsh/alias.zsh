9e309b5fcd87b5eb1a8d285bf39916ea9e536e2e
Sasha Delly <sasha.delly@mail.com>
2016-08-29 22:50:52 +0000

    deduplicated the def of md

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -70,10 +70,6 @@ email() {
 #l() {
 #  pygmentize -O style=sourcerer -f console256 -g $1 | less -r 
 #}
-# read markdown files like manpages
-md() {
-    pandoc -s -f markdown -t man "$*" | man -l -
-}
 # nullpointer url shortener
 short() {
   curl -F"shorten=$*" https://0x0.st
