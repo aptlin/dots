66ea2f03424ffd62494a3d00acdeaff87dc61621
sdll <shura.sdll@gmail.com>
2016-09-03 21:11:43 +0000

    added aliases

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -1,69 +1,68 @@
-alias al="vim ~/.zsh/alias.zsh && source ~/.zsh/alias.zsh; pushd ~/DOTS; git commit -m 'added aliases' ~/DOTS/zsh/.zsh/alias.zsh;  git push; popd"
+#alias emacs="emacs -nw"
+alias "cd.."="cd ../"
+alias :q="sudo systemctl poweroff"
 alias ae="vim ~/.zsh/environment.zsh && source ~/.zsh/environment.zsh; pushd ~/DOTS; git commit -m 'added new env vars' ~/DOTS/zsh/.zsh/environment.zsh;  git push; popd"
-alias ale="ec ~/.zsh/alias.zsh && source ~/.zsh/alias.zsh; pushd ~/DOTS; git commit -m 'added aliases' ~/DOTS/zsh/.zsh/alias.zsh;  git push; popd"
 alias aee="ec ~/.zsh/environment.zsh && source ~/.zsh/environment.zsh; pushd ~/DOTS; git commit -m 'added new env vars' ~/DOTS/zsh/.zsh/environment.zsh;  git push; popd"
-alias dot="cd ~/DOTS"
-alias ll="ls -lahf --color=auto"
-alias lss="ls -hf --color=auto"
-alias lsl="ls -lhf --color=auto"
-alias bach='rsync -aaxv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/boot/*","/opt/*","/usr/*","/bin/*","/sbin"} /home /media/A/REC/home$(date +%m%y)'
+alias al="vim ~/.zsh/alias.zsh && source ~/.zsh/alias.zsh; pushd ~/DOTS; git commit -m 'added aliases' ~/DOTS/zsh/.zsh/alias.zsh;  git push; popd"
+alias ale="ec ~/.zsh/alias.zsh && source ~/.zsh/alias.zsh; pushd ~/DOTS; git commit -m 'added aliases' ~/DOTS/zsh/.zsh/alias.zsh;  git push; popd"
 alias bace='rsync -aaxv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/boot/*","/opt/*","/usr/*","/bin/*","/sbin"} /etc /media/A/REC/etc$(date +%m%y)'
+alias bach='rsync -aaxv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/boot/*","/opt/*","/usr/*","/bin/*","/sbin"} /home /media/A/REC/home$(date +%m%y)'
 alias bacu="bach && bace"
 alias bu="cd /media/A"
+alias clbin="curl -f 'clbin=<-' https://clbin.com"
+alias cp="cp -r"
 alias cv="cd ~/WERKE/ADV/CV/"
+alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e"; echo "╙────────────────────────────────────── ─ ─ "; df -h;'
+alias dot="cd ~/DOTS"
 alias ec="emacsclient -nw --alternate-editor="
 alias ecc="cd ~/.emacs.d && ec ~/.emacs.d/init.el"
 alias ema="cd ~/.emacs.d"
-#alias emacs="emacs -nw"
 alias eth='sudo netctl start ethernet-dhcp'
 alias gb='gemset && sd && rm -r _site && bundle exec jekyll serve'
-alias gt="git add .; read -p 'comment: ' comment; git commit -am '$comment'; git push -u origin master"
 alias gdot="pushd ~/.zsh/ && ./gdot && popd"
+alias genplaylist="cd ~/music;find . -name '*.mp3' -o -name '*.flac'|sed -e 's%^./%%g' > ~/.mpd/playlists/all.m3u"
+alias gt="git add .; read -p 'comment: ' comment; git commit -am '$comment'; git push -u origin master"
 alias hw="cd ~/hw/"
+alias ll="ls -lahf --color=auto"
+alias lsl="ls -lhf --color=auto"
+alias lss="ls -hf --color=auto"
+alias mixer="alsamixer"
+alias mkdir="mkdir -p"
+alias mkdir="mkdir -p"
 alias mou="devmon -a"
+alias nb="workon nikola; cd $BLOG_DIR; nikola auto"
+alias ng="workon nikola; cd $BLOG_DIR; nikola github_deploy -m "Yet another weblog update.""
+alias nt="cd ~/PROG/PIT/nikola/lib/python3.4/site-packages/nikola/data/themes/sdll/templates/"
+alias org='cd ~/ORG'
+alias psef="ps -ef"
 alias ra="ranger"
+alias rbt="sudo reboot"
 alias rd="cd ~/lib/"
+alias rmrf="rm -rf"
 alias ru='setxkbmap ru'
 alias s="cd ~/WERKE/sdll.github.io"
-alias sude="sudo -e"
-alias shtd='shutdown -h now'
-alias st="sudo ntpdate pool.ntp.org; sudo hwclock --systohc"
-alias tm="tmux -2 attach-session -d"
-alias torb='pushd /home/aleph/DLD/tor-browser_en-us && ./start-tor-browser.desktop && popd'
-alias tor='sudo chroot --userspec=tor:tor /opt/torchroot /usr/bin/tor'
-alias transk='sudo killall transmission-daemon'
-alias trans='transmission-daemon && iceweasel http://localhost:9091/'
-alias unm="devmon -u"
-alias updk='sudo mkinitcpio -p linux-libre'
-alias upd='sudo aptitude update && sudo aptitude full-upgrade'
-alias v="jfbview"
-alias wsnet='pushd /home/aleph/h/conf && ./wsnet.sh && popd'
-alias ан='setxkbmap us'
-#source:http://code.xero.nu/dotfiles
-alias "cd.."="cd ../"
-alias up="cd ../"
-alias rmrf="rm -rf"
-alias rbt="sudo reboot"
-alias psef="ps -ef"
-alias mkdir="mkdir -p"
-alias ng="workon nikola; cd $BLOG_DIR; nikola github_deploy -m "Yet another weblog update.""
-alias nb="workon nikola; cd $BLOG_DIR; nikola auto"
-alias nt="cd ~/PROG/PIT/nikola/lib/python3.4/site-packages/nikola/data/themes/sdll/templates/"
-alias cp="cp -r"
 alias scp="scp -r"
-alias mkdir="mkdir -p"
-alias xsel="xsel -b"
-alias fuck='sudo $(fc -ln -1)'
-alias mixer="alsamixer"
-alias genplaylist="cd ~/music;find . -name '*.mp3' -o -name '*.flac'|sed -e 's%^./%%g' > ~/.mpd/playlists/all.m3u"
+alias shtd='shutdown -h now'
+alias sprunge="curl -f 'sprunge=<-' http://sprunge.us"
+alias st="sudo ntpdate pool.ntp.org; sudo hwclock --systohc"
+alias sude="sudo -e"
+alias systemctl="sudo systemctl"
 alias tempwatch="while :; do sensors; sleep 1 && clear; done;"
 alias term="urxvtc -hold -e " #used for run menu
-alias sprunge="curl -f 'sprunge=<-' http://sprunge.us"
-alias clbin="curl -f 'clbin=<-' https://clbin.com"
-alias systemctl="sudo systemctl"
-alias :q="sudo systemctl poweroff"
+alias tm="tmux -2 attach-session -d"
+alias tor='sudo chroot --userspec=tor:tor /opt/torchroot /usr/bin/tor'
+alias torb='pushd /home/aleph/DLD/tor-browser_en-us && ./start-tor-browser.desktop && popd'
+alias trans='transmission-daemon && iceweasel http://localhost:9091/'
+alias transk='sudo killall transmission-daemon'
+alias unm="devmon -u"
+alias up="cd ../"
+alias upd='sudo aptitude update && sudo aptitude full-upgrade'
+alias updk='sudo mkinitcpio -p linux-libre'
+alias v="jfbview"
+alias wsnet='pushd /home/aleph/h/conf && ./wsnet.sh && popd'
+alias xsel="xsel -b"
 alias zz="quit"
-alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e"; echo "╙────────────────────────────────────── ─ ─ "; df -h;'
+alias ан='setxkbmap us'
 email() {
 	echo $3 | mutt -s $2 $1
 }
