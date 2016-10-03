e0d437419ae67f9bf03bdcef6a093c87b082de55
sdll <shura.sdll@gmail.com>
2016-10-03 23:20:10 +0000

    added aliases

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -126,7 +126,7 @@ alias se="pushd $DOTS/zsh/.scripts/"
 
 # Blog
 
-alias nb="workon nikola; cd $BLOG_DIR; nikola auto; deactivate"
+alias nb="workon nikola; nikola auto; deactivate"
 alias ng="workon nikola; cd $BLOG_DIR; nikola github_deploy -m 'Yet another weblog update.'; deactivate"
 alias bd="cd ~/WERKE/sdll.github.io/"
 alias nt="cd ~/PROG/PIT/nikola/lib/python3.4/site-packages/nikola/data/themes/sdll/templates/"
