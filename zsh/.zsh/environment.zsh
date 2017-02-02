4060c039fbb79ab17f18236b4649895ab6bdd994
sdll <shura.sdll@gmail.com>
2017-02-02 21:26:46 +0000

    added new env vars

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -8,7 +8,7 @@ export ZSH=$HOME/.oh-my-zsh
 #HIST_STAMPS=mm/dd/yyyy
 
 # paths
-export PATH=$PYENV_ROOT:/bin:$HOME/bin:/usr/local/bin:$PATH:/$HOME/PROG/SageMath/:/$HOME/node_modules/i3-run-or-raise/bin/
+export PATH=$PYENV_ROOT:/bin:$HOME/bin:/usr/local/bin:$PATH:/$HOME/.scripts/:/$HOME/PROG/SageMath/:/$HOME/node_modules/i3-run-or-raise/bin/
 CDPATH=.:~=:~/WERKE:~/ORG:~/
 for folder in ~/WERKE
 do
