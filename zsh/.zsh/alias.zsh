e5442c96b55884683692489509c5711ef08fae47
sdll <shura.sdll@gmail.com>
2017-04-21 03:03:20 +0000

    added aliases

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -149,7 +149,7 @@ alias mik="ec $BLOG_DIR/stories/mikveh.org"
 alias hmu="pkill -2 -u $UID mu # send SIGINT"
 alias we="curl wttr.in"
 function ns() { nix-env -qa \* -P | fgrep -i "$1"; }
-alias ni="ec /etc/nixos/configuration.nix; sudo nixos-rebuild switch"
+alias ni="ec ~/DOTS/nixos/configuration.nix; sudo nixos-rebuild switch"
 alias nb="sudo nixos-rebuild switch"
 # Config
 
