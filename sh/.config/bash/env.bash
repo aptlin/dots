722d4fc4d5afbe7f931b37365aac6095418fad73
sdll <shura.sdll@gmail.com>
2017-09-03 16:41:02 +0300

    added rdm config

--- sh/.config/bash/env.bash
+++ sh/.config/bash/env.bash
@@ -2,3 +2,5 @@ XDG_CONFIG_HOME=/home/sdll/.config
 export EDITOR="emacsclient -c --alternate-editor= "
 export PATH=~/bin:$PATH
 export WORKON_HOME=~/dev/ml/
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
